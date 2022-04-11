a10dcdb180894dab0ea8bbe60f34700863988016
Valkiria0000 <90210008+Valkiria0000@users.noreply.github.com>
2022-04-11 16:34:42 +0300

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,10 +1,6 @@
 //Задание 3 модуля 7
 //Написать функцию, которая создает пустой объект, но без прототипа.
-const STUDENT = {
-  name: 'Irina',
-  surname: 'Ignatenko',
-  city: 'Moscow'
-}
+
 function createObj() {
   let student = Object.create(null);
   console.log(student); 
